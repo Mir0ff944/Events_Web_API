507c82f6823cd62a99d0e54df41f44a683d5c467
Miroslav <housera94@gmail.com>
2016-11-17 17:14:33 +0000

    artists + events search

--- Modules/eventful.js
+++ Modules/eventful.js
@@ -13,29 +13,27 @@ request.get({url: url, qs: query_string}, (err, res, body) => {
 		if (err) {
 			throw err
 		} else {
-      //console.log(body)
+			console.log(body)
 			const json = JSON.parse(body)
 			if (json.cod !== globals.ok) {
-
-				return {
-					status: globals.status.notFound,
-					format: globals.format.json,
-					message: 'list not found'
-				}
-
+				throw globals.status.notFound
 			}
-			const events = JSON.stringify(json, null, 2)
+
+			//const array = JSON.stringify(json, null, 2)
 			console.log('STATUS CODE: '+res.statusCode)
-			//console.log(events)
+			//console.log(array)
 			console.log('Total Items: ' + json.total_items)
-			console.log('################')
-			for (let i = 0; i <= json.total_items; i++) {
-				console.log(json.events.event[i].title)
-				console.log(json.events.event[i].start_time)
-				console.log(json.events.event[i].city_name)
-				console.log(json.events.event[i].country_name)
-				console.log(json.events.event[i].id)
-				console.log('################')
+			console.log('––––––––––––––––––––––––––––––––')
+			for (let i = 0; i < json.events.event.length ; i++) {
+				console.log('Title: '+json.events.event[i].title)
+				console.log('Starts at: '+json.events.event[i].start_time)
+				console.log('Venue: '+json.events.event[i].venue_name)
+				console.log('City: '+json.events.event[i].city_name)
+				console.log('Region: '+json.events.event[i].region_name)
+				console.log('Country: '+json.events.event[i].country_name)
+				console.log('Event link: '+json.events.event[i].url)
+				console.log('Event ID: '+json.events.event[i].id)
+				console.log('––––––––––––––––––––––––––––––––')
 			}
 		}
 	} catch(err) {
